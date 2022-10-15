18375bbb34c1bc1095e8926fe2439d3079e4e72a
nand2mario <nand2mario@outlook.com>
2022-10-15 13:54:34 +0800

    Add memory r/w testing at startup: ddr3_tester

--- src/memory_controller.v
+++ src/memory_controller.v
@@ -1,11 +1,17 @@
-// Connects DDR3 controller to NES
-
+// A bridge controller connecting DDR3 to NES.
+// nand2mario, 2022.10
+// 
 // Memory layout:
 // Total address space 4MB
-// $00_0000 - $0f_ffff: PRG ROM 1MB
-// $20_0000 - $2f_ffff: CHR ROM 1MB
+// $00_0000 - $1f_ffff: PRG ROM 2MB
+// $20_0000 - $37_ffff: CHR ROM 1.5MB
 // $38_0000 - $38_07ff: Internal RAM (2KB), and 126KB unused
 // $3c_0000 - $3d_ffff: PRG RAM 128KB
+//
+// This also uses ddr3_tester to verify that the DDR3 is initialized and read/write actually 
+// works. If not, busy will stay 1 and fail_high/fail_low will be 1. The user can choose to 
+// reset the controller to try initialization again. 
+//
 module MemoryController(
     input clk,                // Main logic clock (1/3 of pclk)
     input pclk,               // DDR3 controller clock 96.75Mhz
@@ -21,26 +27,32 @@ module MemoryController(
     output reg [7:0] dout_a,  // Last read data a, available 2 cycles after read_a is set
     output reg [7:0] dout_b,  // Last read data b, available 2 cycles after read_b is set
     output reg busy,          // 1 while an operation is in progress
-    output reg fail,          // timing mistake or sdram malfunction detected
-
-    // debug interface
-    output reg [19:0] total_written,
-    output reg [7:0] debug,
-    output write_level_done,
+                              // staying 1 for more than 10ms means initialization or testing has failed
+                              // if this happens, check: write_level_done, read_calib_done, fail_high, fail_low
+    output write_level_done,  // write leveling status, this is 1st part of initialization
     output [7:0] wstep,
-    output read_calib_done,
+    output read_calib_done,   // read calibration status, this is 2nd half of initialization
     output [1:0] rclkpos,
     output [2:0] rclksel,
+    output testing,
+    output [3:0] test_state,
+    output fail_high,         // error in higher byte r/w testing. 1 if initialization succeeds but r/w testing fails
+    output fail_low,         
+    output reg [19:0] total_written,
+
+    // debug interface
+    output reg [7:0] debug,
+    output reg fail,          // FIXME: fail pin is currently not useful
 
     // Physical DDR3 interface
-    inout  [15:0] DDR3_DQ,   // 16 bit bidirectional data bus
-    inout  [1:0] DDR3_DQS,   // DQ strobe for high and low bytes
-    output [13:0] DDR3_A,    // 14 bit multiplexed address bus
-    output [2:0] DDR3_BA,    // 3 banks
-    output DDR3_nCS,  // a single chip select
-    output DDR3_nWE,  // write enable
-    output DDR3_nRAS, // row address select
-    output DDR3_nCAS, // columns address select
+    inout  [15:0] DDR3_DQ,    // 16 bit bidirectional data bus
+    inout  [1:0] DDR3_DQS,    // DQ strobe for high and low bytes
+    output [13:0] DDR3_A,     // 14 bit multiplexed address bus
+    output [2:0] DDR3_BA,     // 3 banks
+    output DDR3_nCS,          // chip select
+    output DDR3_nWE,          // write enable
+    output DDR3_nRAS,         // row address select
+    output DDR3_nCAS,         // columns address select
     output DDR3_CK,
     output DDR3_nRESET,
     output DDR3_CKE,
@@ -48,14 +60,21 @@ module MemoryController(
     output [1:0] DDR3_DM
 );
 
+localparam INIT = 2'd0;
+localparam TEST = 2'd1;
+localparam NORMAL = 2'd2;
+
 reg [25:0] MemAddr;
-reg MemRD, MemWR, MemRefresh, MemInitializing;
+reg [1:0] state;
+reg MemRD, MemWR, MemRefresh;
 reg [15:0] MemDin;
 wire [15:0] MemDout;
 reg [2:0] cycles;
 reg r_read_a, r_read_b;
 wire MemBusy, MemDataReady;
-
+wire fail_high, fail_low, tester_rd, tester_wr, tester_refresh, testing;
+wire [25:0] tester_addr;
+wire [15:0] tester_din;
 
 `ifndef VERILATOR
 
@@ -64,10 +83,12 @@ ddr3_controller #(
     .ROW_WIDTH(13), .COL_WIDTH(10), .BANK_WIDTH(3)
 ) u_ddr3 (
     .pclk(pclk), .fclk(fclk), .ck(ck), .resetn(resetn),
-	.addr(MemAddr), .rd(MemRD), .wr(MemWR), .refresh(MemRefresh),
-	.din(MemDin), .dout(MemDout), .busy(MemBusy), .data_ready(MemDataReady),
-    .write_level_done(write_level_done), .wstep(wstep),       // write leveling status
-    .read_calib_done(read_calib_done), .rclkpos(rclkpos), .rclksel(rclksel),        // read calibration status
+	.addr(testing ? tester_addr : MemAddr), .rd(testing ? tester_rd : MemRD), 
+    .wr(testing ? tester_wr : MemWR), .refresh(testing ? tester_refresh : MemRefresh),
+	.din(testing ? tester_din : MemDin), .dout(MemDout), 
+    .data_ready(MemDataReady), .busy(MemBusy),              
+    .write_level_done(write_level_done), .wstep(wstep),     
+    .read_calib_done(read_calib_done), .rclkpos(rclkpos), .rclksel(rclksel),        
 
     .DDR3_nRESET(DDR3_nRESET),
     .DDR3_DQ(DDR3_DQ),      // 16 bit bidirectional data bus
@@ -82,6 +103,15 @@ ddr3_controller #(
     .DDR3_CKE(DDR3_CKE),
     .DDR3_ODT(DDR3_ODT),
     .DDR3_DM(DDR3_DM)
+);
+
+ddr3_tester u_tester (
+    .clk(clk), .resetn(resetn), .start(~MemBusy), .running(testing), .state(test_state),
+    .fail_high(fail_high), .fail_low(fail_low),
+
+    .rd(tester_rd), .wr(tester_wr), .refresh(tester_refresh),
+    .addr(tester_addr), .din(tester_din),
+    .dout(MemDout), .data_ready(MemDataReady), .busy(MemBusy)
 );
 
 always @(posedge clk) begin
@@ -103,12 +133,12 @@ always @(posedge clk) begin
 
             if (write) total_written <= total_written + 1;
         end 
-    end else if (MemInitializing) begin
-        if (~MemBusy) begin
-            // initialization is done
-            MemInitializing <= 1'b0;
-            busy <= 1'b0;
-        end
+    end else if (state == INIT && ~MemBusy) begin
+        // initialization is done, now test memory read/write
+        state <= TEST;
+    end else if (state == TEST && ~testing) begin
+        state <= NORMAL;
+        busy <= 1'b0;
     end else begin
         // Wait for operation to finish and latch incoming data on read.
         if (cycles == 3'd4) begin
@@ -133,7 +163,7 @@ always @(posedge clk) begin
         busy <= 1'b1;
         fail <= 1'b0;
         total_written <= 0;
-        MemInitializing <= 1'b1;
+        state <= INIT;
     end
 end
 
--- src/nes_tang20k.v
+++ src/nes_tang20k.v
@@ -275,6 +275,7 @@ module NES_Tang20k(
   wire [7:0] ram_debug, wstep;
   wire [1:0] rclkpos;
   wire [2:0] rclksel;
+  wire [3:0] test_state;
   MemoryController memory(.clk(clk), .pclk(pclk), .fclk(fclk), .ck(ck), .resetn(sys_resetn & nes_lock & mem_resetn),
         .read_a(memory_read_cpu && run_mem), 
         .read_b(memory_read_ppu && run_mem),
@@ -287,6 +288,7 @@ module NES_Tang20k(
         .busy(ram_busy), .fail(ram_fail), .total_written(ram_total_written),
         .debug(ram_debug), .write_level_done(write_level_done), .wstep(wstep),
         .read_calib_done(read_calib_done), .rclkpos(rclkpos), .rclksel(rclksel),
+        .testing(ram_testing), .fail_high(fail_high), .fail_low(fail_low), .test_state(test_state),
 
         .DDR3_DQ(DDR3_DQ), .DDR3_DQS(DDR3_DQS), .DDR3_A(DDR3_A), .DDR3_BA(DDR3_BA), 
         .DDR3_nCS(DDR3_nCS), .DDR3_nWE(DDR3_nWE), .DDR3_nRAS(DDR3_nRAS), .DDR3_nCAS(DDR3_nCAS), 
@@ -323,14 +325,16 @@ nes2hdmi u_hdmi (
 );
 
 // Memory initialization control
-// After reset, every 100ms we check whether memory is successsfully intialized.
+// After reset, every 250ms we check whether memory is successsfully intialized and tested.
 // If not we reset the whole machine, print a message and hope it will finally succeed.
-reg [$clog2(FREQ/10+1)-1:0] meminit_cnt;
+reg [$clog2(FREQ/4+1)-1:0] meminit_cnt;
 reg meminit_check;      // pulse when memory initialization is checked
 reg write_level_done_p, read_calib_done_p;
 reg [7:0] wstep_p;
 reg [1:0] rclkpos_p;
 reg [2:0] rclksel_p;
+reg ram_busy_p, ram_testing_p, fail_high_p, fail_low_p;
+
 always @(posedge clk) begin
     meminit_cnt <= meminit_cnt == 0 ? 0 : meminit_cnt - 1;
     mem_resetn <= 1'b1;
@@ -339,15 +343,17 @@ always @(posedge clk) begin
         meminit_check <= 1'b1;
         write_level_done_p <= write_level_done; read_calib_done_p <= read_calib_done;
         wstep_p <= wstep; rclkpos_p <= rclkpos; rclksel_p <= rclksel;
-        if (~write_level_done || ~read_calib_done) begin
+        ram_busy_p <= ram_busy; fail_high_p <= fail_high; fail_low_p <= fail_low;
+        ram_testing_p <= ram_testing;
+        if (~write_level_done || ~read_calib_done || fail_high || fail_low) begin
             // reset DDR3 controller
             mem_resetn <= 0;
-            meminit_cnt <= FREQ/10;         // check again in 0.1 sec
+            meminit_cnt <= FREQ/4;         // check again in 0.25 sec
         end
     end
 
     if (~sys_resetn || ~nes_lock) begin
-        meminit_cnt <= FREQ/10;
+        meminit_cnt <= FREQ/4;
         meminit_check <= 1'b0;
         mem_resetn <= 1'b1;
     end
@@ -398,11 +404,11 @@ always@(posedge clk)begin
     print_mem_p <= print_mem;
     if (print_state == PRINT_IDLE_STATE && print_mem == print_mem_p && print_mem != 0) begin
         case (print_mem)
-        8'd1: if (write_level_done_p && read_calib_done_p) 
-                `print("DDR3 initialization successful.\n{wstep[7:0],rclkpos[3:0],rclksel[3:0]}=", STR);
+        8'd1: if (write_level_done_p && read_calib_done_p && ~fail_high_p && ~fail_low_p) 
+                `print("DDR3 initialization successful.\n{wstep[7:0],rclkpos[3:0],rclksel[3:0],fail_high[3:0],fail_low[3:0]}=", STR);
               else
-                `print("DDR3 initialization failed. Retrying...\n{wstep[7:0],rclkpos[3:0],rclksel[3:0]}=", STR);
-        8'd2: `print({wstep_p,2'b0,rclkpos_p, 1'b0, rclksel_p}, 2);
+                `print("DDR3 initialization failed. Retrying...\n{wstep[7:0],rclkpos[3:0],rclksel[3:0],fail_high[3:0],fail_low[3:0]}=", STR);
+        8'd2: `print({wstep_p,2'b0,rclkpos_p, 1'b0, rclksel_p, 3'b0, fail_high_p, 3'b0, fail_low_p}, 3);        // 3 bytes
         8'd3: `print("\n", STR);
         endcase
         print_mem <= print_mem == 8'd4 ? 0 : print_mem + 1;
@@ -481,7 +487,7 @@ end
 
 
 //  assign led = ~{loader_done, ram_busy, uart_error, 5'b0};
- assign led = ~{loader_done, ram_busy, uart_error, color[4:0]};
+ assign led = ~{loader_done, ram_busy, uart_error, ram_testing, test_state};
 //  assign led2 = ~sample[15:8];
 //  assign led2 = ~{6'b0, ram_fail, ram_busy};
   assign led2 = ~loader_addr[15:8];
