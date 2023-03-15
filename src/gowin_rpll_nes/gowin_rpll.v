86e350c9bb12c7092e2f5c14676132d73a4a61fb
nand2mario <nand2mario@outlook.com>
2023-03-15 13:30:42 +0800

    fix pll

--- src/gowin_rpll_nes/gowin_rpll.v
+++ src/gowin_rpll_nes/gowin_rpll.v
@@ -6,16 +6,16 @@
 //Device: GW2A-18C
 //Created Time: Fri May 06 23:06:50 2022
 
-module Gowin_rPLL_nes (clkout, clkoutp, clkoutd, clkoutd3, lock, reset, clkin);
+module Gowin_rPLL_nes (clkout, clkoutp, lock, reset, clkin);
 
 output clkout;
 output clkoutp;
 output lock;
 input reset;
 input clkin;
-output clkoutd;
-output clkoutd3;
 
+wire clkoutd_o;
+wire clkoutd3_o;
 wire gw_gnd;
 
 assign gw_gnd = 1'b0;
@@ -24,8 +24,8 @@ rPLL rpll_inst (
     .CLKOUT(clkout),
     .LOCK(lock),
     .CLKOUTP(clkoutp),
-    .CLKOUTD(clkoutd),
-    .CLKOUTD3(clkoutd3),
+    .CLKOUTD(clkoutd_o),
+    .CLKOUTD3(clkoutd3_o),
     .RESET(reset),
     .RESET_P(gw_gnd),
     .CLKIN(clkin),
@@ -38,32 +38,30 @@ rPLL rpll_inst (
     .FDLY({gw_gnd,gw_gnd,gw_gnd,gw_gnd})
 );
 
-// Clock targets:
-// NES PPU clock:               nclk = 5.369 Mhz
-// Main clock:                  clk  = 5.369 * 6 = 32.214 Mhz
-// DDR3 controller clock:       pclk = clk * 3 = 96.642 Mhz
-// DDR3 memory clock:           fclk = pclk * 4 = 386.568 Mhz
+// Target main clock: 5.369 * 7 = 37.583 Mhz
 
-// fclk 387Mhz, 0.11% deviation
-defparam rpll_inst.FBDIV_SEL = 42;
-defparam rpll_inst.IDIV_SEL = 2;
-defparam rpll_inst.ODIV_SEL = 2;
-
-// DDR3_CK (clkoutp) is 90-degree phase shifted fclk
-defparam rpll_inst.PSDA_SEL = "0100";
-
-// pclk (clkoutd) is 1/4 of fclk
-defparam rpll_inst.DYN_SDIV_SEL = 4;
-defparam rpll_inst.CLKOUTD_SRC = "CLKOUT";
-
-// clkoutd3 is 1/3 of fclk (used later to get main clock)
-defparam rpll_inst.CLKOUTD3_SRC = "CLKOUT";
-
-// Other parameters
+// 27 -> 37.8 Mhz, 0.6% deviation
 defparam rpll_inst.FCLKIN = "27";
+defparam rpll_inst.IDIV_SEL = 4;
+defparam rpll_inst.FBDIV_SEL = 6;
+defparam rpll_inst.ODIV_SEL = 16;
+
+// 62.5 -> 37.5
+//defparam rpll_inst.FCLKIN = "62.5";
+//defparam rpll_inst.IDIV_SEL = 4;
+//defparam rpll_inst.FBDIV_SEL = 2;
+//defparam rpll_inst.ODIV_SEL = 16;
+
+// 27 -> 27 Mhz
+//defparam rpll_inst.FCLKIN = "27";
+//defparam rpll_inst.IDIV_SEL = 4;   // IDIV is [0 ~ 8]
+//defparam rpll_inst.FBDIV_SEL = 4;
+//defparam rpll_inst.ODIV_SEL = 32;
+
 defparam rpll_inst.DYN_IDIV_SEL = "false";
 defparam rpll_inst.DYN_FBDIV_SEL = "false";
 defparam rpll_inst.DYN_ODIV_SEL = "false";
+defparam rpll_inst.PSDA_SEL = "1000";
 defparam rpll_inst.DYN_DA_EN = "false";
 defparam rpll_inst.DUTYDA_SEL = "1000";
 defparam rpll_inst.CLKOUT_FT_DIR = 1'b1;
@@ -74,6 +72,9 @@ defparam rpll_inst.CLKFB_SEL = "internal";
 defparam rpll_inst.CLKOUT_BYPASS = "false";
 defparam rpll_inst.CLKOUTP_BYPASS = "false";
 defparam rpll_inst.CLKOUTD_BYPASS = "false";
-defparam rpll_inst.DEVICE = "GW2A-18C";
+defparam rpll_inst.DYN_SDIV_SEL = 2;
+defparam rpll_inst.CLKOUTD_SRC = "CLKOUT";
+defparam rpll_inst.CLKOUTD3_SRC = "CLKOUT";
+defparam rpll_inst.DEVICE = "GW2AR-18C";
 
 endmodule //Gowin_rPLL
