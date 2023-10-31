763b419cea617267e1c4044bce24621aa3cba8c3
nand2mario <113511414+nand2mario@users.noreply.github.com>
2023-11-01 00:14:26 +0800

    Update nano20k.tcl

--- nano20k.tcl
+++ nano20k.tcl
@@ -7,4 +7,6 @@ add_file src/tang_nano_20k/gowin_pll_usb.v
 add_file -type cst "src/tang_nano_20k/nestang.cst"
 add_file -type sdc "src/tang_nano_20k/nestang.sdc"
 
+set_option -use_sspi_as_gpio 1
+
 source build.tcl
