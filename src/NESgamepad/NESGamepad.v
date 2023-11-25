6332797b180dafc0649957f54f0824088cb8594d
fjpolo <fjpolo@gmail.com>
2023-11-25 19:13:04 +0100

    - Add formal verification

--- src/NESgamepad/NESGamepad.v
+++ src/NESgamepad/NESGamepad.v
@@ -1,6 +1,5 @@
 // NES classic gamepad based on https://github.com/michael-swan/NES-Controller-SIPO
 // fjpolo, 11.2023
-parameter NUMBER_OF_STATES = 10;
 
 module NESGamepad(
 		input i_clk,
@@ -13,6 +12,8 @@ module NESGamepad(
 		output reg [7:0] o_button_state,
 		output o_data_available
    );
+	parameter NUMBER_OF_STATES = 10;
+	
 	// Unit Parameters //
 	parameter Hz  = 1;
 	parameter KHz = 1000*Hz;
@@ -22,6 +23,7 @@ module NESGamepad(
 	parameter MASTER_CLOCK_FREQUENCY = 27*MHz; // USER VARIABLE
 	parameter OUTPUT_UPDATE_FREQUENCY = 120*Hz; // USER VARIABLE
     parameter LATCH_CYCLES = (12 / 1000000) * (1 / MASTER_CLOCK_FREQUENCY);
+	parameter LATCH_12US_CYCLES = 324;
 	
 	// Clock divider register size
 	// parameter DIVIDER_EXPONENT = log2( (MASTER_CLOCK_FREQUENCY / OUTPUT_UPDATE_FREQUENCY) / 10 ) - 2;
@@ -31,9 +33,6 @@ module NESGamepad(
 	reg [(NUMBER_OF_STATES-1):0] cycle_stage;
     reg [31:0] latch_counter;
 	reg [7:0] data;
-
-    initial cycle_stage = 1;
-	initial latch_counter = 0;
 
 	// Generate control signals for the three phases
 	// wire latch_phase = cycle_stage[0];
@@ -45,49 +44,136 @@ module NESGamepad(
 	// Generate a clock for generating the data clock and sampling the controller's output
 	reg [DIVIDER_EXPONENT:0] sample_count;
 	wire sample_clock = sample_count[DIVIDER_EXPONENT];
+
+    initial cycle_stage = 1;
+	initial latch_counter = 0;
+	initial data = 8'h00;
+	initial sample_count = 0;
+
+	// Handle latch counter -> 0...324 * i_clk = ~12uS
 	always @(posedge i_clk) begin
 		if (i_rst) begin
-            sample_count <= 0;
             latch_counter <= 0;
         end else begin               
             if (latch_phase) begin
-				o_button_state <= 0;
-                latch_counter <= latch_counter + 1;
+				if (latch_counter > (LATCH_12US_CYCLES - 1))
+					latch_counter <= 32'h0000_0000;
+                else
+					latch_counter <= latch_counter + 1;
             end else if (cycle_stage > 1) begin
                 latch_counter <= 32'h0000_0000;
             end else begin
                 latch_counter <= 32'hFFFF_FFFF;
             end
-			// o_button_state
-			if (end_phase) begin
-				o_button_state[7:0] <= data;
-			end else begin
-				o_button_state[7:0] <= o_button_state[7:0];
-			end
-            sample_count <= sample_count + 1;
         end
     end
+	
+	// Handle sample count
+	always @(posedge i_clk)
+		if (i_rst)
+            sample_count <= 0;
+		else
+			sample_count <= sample_count + 1;
+
+	// Handle button output
+	always @(posedge i_clk) begin          
+		// o_button_state
+		if (latch_phase) begin
+				o_button_state <= 0;
+		end if (end_phase) begin
+			o_button_state[7:0] <= data;
+		end else begin
+			o_button_state[7:0] <= o_button_state[7:0];
+		end
+	end
 	
 	// Handle inputs from the controller
 	always @(posedge sample_clock) begin
         if (i_rst) begin
             data <= 0;
-            cycle_stage <= 1;
         end else begin
             if(latch_phase) begin
                 data <= 8'h0;
             end else if(data_phase) begin
-                data <= {data[6:0], i_serial_data};
+                // data <= {data[6:0], i_serial_data};	        // [    A       B       Select  Start   Up      Down    Left    Right   ]
+                data <= {i_serial_data, data[7:1]};		        // [    Right   Left    Down    Up      Start   Select  B       A       ]
             end
-            // Count cycles
-			if(cycle_stage == (1<<(NUMBER_OF_STATES-1)))
-				cycle_stage <= 1;
-			else
-				cycle_stage <= cycle_stage << 1;
         end
 	end
 
+	// Handle cycle stage
+	always @(posedge sample_clock) begin
+		if (i_rst) begin
+            cycle_stage <= 1;
+        end else begin
+			// Count cycles
+			if(cycle_stage == (1<<(NUMBER_OF_STATES - 1)))
+				cycle_stage <= 1;
+			else
+				cycle_stage <= cycle_stage << 1;
+		end
+	end
+
 	// Generate output signals
-	assign o_data_latch = latch_phase & (latch_counter <= 324);
+	assign o_data_latch = latch_phase & (latch_counter <= LATCH_12US_CYCLES);
 	assign o_data_clock = data_phase & sample_clock;
+
+	//
+	// Formal verification
+	//
+	`ifdef	FORMAL
+		always @(*) begin
+			if(latch_phase) begin
+				assert( (latch_phase != data_phase) );
+				assert( (latch_phase != end_phase) );
+			end
+			if(data_phase) begin
+				assert( (data_phase != latch_phase) );
+				assert( (data_phase != end_phase) );
+			end
+			if(end_phase) begin
+				assert( (end_phase != latch_phase) );
+				assert( (end_phase != latch_phase) );
+			end
+		end
+
+		always @(*)
+			assert((latch_counter <= LATCH_12US_CYCLES) || (latch_counter == 32'hFFFF_FFFF));
+		
+		// always @(posedge i_clk) begin
+		// 	if (i_rst) begin
+		// 		assert(sample_count == 0);
+		// 		assert(latch_counter == 0);
+		// 	end
+		// end
+
+		always @(posedge sample_clock) begin
+			if (i_rst) begin
+				assert(data == 0);
+				assert(cycle_stage == 1);
+			end
+		end
+				
+
+		reg	f_valid_state;
+		always @(*) begin
+			f_valid_state = 0;
+			case(cycle_stage)
+				10'h01: f_valid_state = 1'b1;
+				10'h02: f_valid_state = 1'b1;
+				10'h04: f_valid_state = 1'b1;
+				10'h08: f_valid_state = 1'b1;
+				10'h10: f_valid_state = 1'b1;
+				10'h20: f_valid_state = 1'b1;
+				10'h40: f_valid_state = 1'b1;
+				10'h80: f_valid_state = 1'b1;
+				10'h100: f_valid_state = 1'b1;
+				10'h200: f_valid_state = 1'b1;
+			endcase
+			assert(f_valid_state);
+		end
+
+	`endif
+
+
 endmodule
--- src/NESgamepad/obj_dir/NESGamepad.cpp
+++ src/NESgamepad/obj_dir/NESGamepad.cpp
@@ -38,7 +38,7 @@
 
 #define MASTER_CLOCK_FREQUENCY      27000000
 #define OUTPUT_UPDATE_FREQUENCY     120
-#define SERIAL_DATA_COUNTER         2 * 8191
+#define i_serial_data_COUNTER         2 * 8191
 #define NUMBER_OF_STATES (10)
 #define LATCH_STATE (1)
 #define READ_STATE (1 << (NUMBER_OF_STATES-1))
@@ -94,10 +94,9 @@ int main(int argc, char **argv) {
  
 
     tb->i_clk = 0x00;
-    tb->data_clock = 0x00;
-    tb->serial_data = 0x01;
+    tb->i_serial_data = 0x01;
     tb->i_rst = 0x00;
-    static int serial_data_counter = SERIAL_DATA_COUNTER;
+    static int i_serial_data_counter = i_serial_data_COUNTER;
     static int FSM_state = LATCH_STATE;
     static bool create_new_data_value = false;
     #ifdef PRINT_LATCH_STATE
@@ -106,41 +105,31 @@ int main(int argc, char **argv) {
     for(int k=0; k<(1<<19); k++){
         // Tick()
         tick(++tick_count, tb, tfp);
-
-        // 
-        // if(tb->i_clk <= 128)
-        //     tb->i_rst = 0x01;
-        // else
-        //     tb->i_rst = 0x00;
-
-        //
-        // printf("k= %7d | i_clk= %d | o_clk= %d | i_rst= %u | data_latch= %d | o_latch_counter= %d | data= %d | o_cycle_stage: %d | button_state= %d\n", 
-        // k, tb->i_clk, tb->o_clk, tb->i_rst, tb->data_latch, tb->o_latch_counter, joystick_serial, tb->o_cycle_stage, tb->button_state);
         
-        serial_data_counter--;
-        if(!serial_data_counter && !tb->data_latch){
+        i_serial_data_counter--;
+        if(!i_serial_data_counter && !tb->o_data_latch){
             // FSM_state
             FSM_state = FSM_state << 1;
             if(FSM_state > READ_STATE)
                 FSM_state = LATCH_STATE;
             
-            serial_data_counter = SERIAL_DATA_COUNTER;
-            // tb->serial_data = (tb->serial_data == 0x00) ? 0x01 : 0x00;
+            i_serial_data_counter = i_serial_data_COUNTER;
+            // tb->i_serial_data = (tb->i_serial_data == 0x00) ? 0x01 : 0x00;
 
             if(FSM_state != LATCH_STATE && FSM_state != READ_STATE){
 
                 joystick_serial_bit = joystick_serial & 0x80;
-                tb->serial_data = joystick_serial >> 7 ;
+                tb->i_serial_data = joystick_serial >> 7 ;
                 joystick_serial = (uint8_t)(joystick_serial << 1);
             }
         }
 #ifdef PRINT_LATCH_STATE
         // Latch state
-        if(tb->data_latch){
+        if(tb->o_data_latch){
             //
             if(!latch_info_was_printed){
-            printf("Latch state\n\tk= %7d | i_clk= %d | o_clk= %d | i_rst= %u | data_latch= %d | o_latch_counter= %d | data= %d | o_cycle_stage: %d | button_state= %d\n", 
-            k, tb->i_clk, tb->o_clk, tb->i_rst, tb->data_latch, tb->o_latch_counter, joystick_serial, tb->o_cycle_stage, tb->button_state);
+            printf("Latch state\n\tk= %7d | i_clk= %d | i_rst= %u | o_data_latch= %d | data= %d | o_button_state= %d\n", 
+            k, tb->i_clk, tb->i_rst, tb->o_data_latch, joystick_serial, tb->o_button_state);
             latch_info_was_printed = true;
             }
         }
@@ -149,13 +138,10 @@ int main(int argc, char **argv) {
 #endif
 
         // Data out state
-        if(tb->o_cycle_stage == READ_STATE){
+        if(tb->o_data_available){
             if(!create_new_data_value){
-                // printf("Output state\n\tk= %7d | i_clk= %d | o_clk= %d | i_rst= %d | data_latch= %u | o_cycle_stage: %d | button_state= %d\n", 
-                //     k, tb->i_clk, tb->o_clk, tb->i_rst, tb->data_latch, tb->o_cycle_stage, tb->button_state);
                 create_new_data_value = true;
                 joystick_serial = dist255(rng);
-                // printf("Next data= %u\n", joystick_serial);
             }
         }
         else
