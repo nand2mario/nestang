a9e805cf57c754e7d7c14737208abb340223852e
nand2mario <nand2mario@outlook.com>
2024-03-29 23:44:35 +0800

    iosys: fixes

---
+++ src/verilator/config.sv
@@ -0,0 +1,11 @@
+`define RES_720P
+`define GW_IDE
+
+package configPackage;  
+
+    localparam SDRAM_DATA_WIDTH = 16;
+    localparam SDRAM_ROW_WIDTH = 13;
+    localparam SDRAM_COL_WIDTH = 9;
+    localparam SDRAM_BANK_WIDTH = 2;
+
+endpackage
