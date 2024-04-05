34998404c051cccf6001931efd006f70b8243908
nand2mario <nand2mario@outlook.com>
2024-04-05 12:24:42 +0800

    fix tcl build files

--- primer25k.tcl
+++ primer25k.tcl
@@ -3,7 +3,8 @@ set dev "primer25k"
 add_file src/tang_primer_25k/config.sv
 add_file src/tang_primer_25k/gowin_pll_27.v
 add_file src/tang_primer_25k/gowin_pll_hdmi.v
-add_file src/tang_primer_25k/gowin_pll_usb.v
+add_file src/tang_primer_25k/gowin_pll_nes.v
+# add_file src/tang_primer_25k/gowin_pll_usb.v
 add_file -type cst "src/tang_primer_25k/nestang.cst"
 add_file -type sdc "src/tang_primer_25k/nestang.sdc"
 
