7b2b4e7b0b411d9deda5b6bb948057b43ea893d5
nand2mario <nand2mario@outlook.com>
2025-03-07 21:50:20 +0800

    fix build file

--- buildall.bat
+++ buildall.bat
@@ -7,6 +7,7 @@ echo.
 echo ============ Building nano20k ===============
 echo.
 %GWSH% build.tcl nano20k bl616
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 @REM echo.
 @REM echo ============ Building primer25k with snes/nes controller ===============
@@ -17,16 +18,25 @@ echo.
 echo ============ Building primer25k with ds2 controller ===============
 echo.
 %GWSH% build.tcl primer25k ds2
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 echo.
 echo ============ Building mega60k with ds2 controller ===============
 echo.
 %GWSH% build.tcl mega60k ds2
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo.
+echo ============ Building mega138k with ds2 controller ===============
+echo.
+%GWSH% build.tcl mega138k ds2
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 echo.
 echo ============ Building console60k with ds2 controller ===============
 echo.
 %GWSH% build.tcl console60k ds2
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 dir impl\pnr\*.fs
 
